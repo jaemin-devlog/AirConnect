4ebf586f88a0ce2804b7988d1c72cb550087a56b
재민 <jjm0203311@naver.com>
2026-03-04 18:30:26 +0900

    init

---
+++ src/main/java/univ/airconnect/AirConnectApplication.java
@@ -0,0 +1,13 @@
+package univ.airconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/univ/airconnect/AirConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package univ.airconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
